cc96743cb58c0f68ee3f71bf75cccad34e7091b3
vishnu1264 <vishnu.kundhan@mobigesture.com>
2022-04-06 04:05:30 +0530

    UI-Pages

---
+++ index.js
@@ -0,0 +1,36 @@
+let loginPage1 = document.getElementById("loginPage1");
+let loginPage2 = document.getElementById("loginPage2");
+let signupForm = document.getElementById("signupForm");
+let dashboard = document.getElementById("dashboard");
+
+function accessLogin1()
+{
+    loginPage1.style.display = "block";
+    loginPage2.style.display = "none";
+    signupForm.style.display = "none";
+    dashboard.style.display = "none";
+}
+
+function accessLogin2()
+{
+    loginPage1.style.display = "none";
+    loginPage2.style.display = "block";
+    signupForm.style.display = "none";
+    dashboard.style.display = "none";
+}
+
+function accessSignup()
+{
+    loginPage1.style.display = "none";
+    loginPage2.style.display = "none";
+    signupForm.style.display = "block";
+    dashboard.style.display = "none";
+}
+
+function accessDashboard()
+{
+    loginPage1.style.display = "none";
+    loginPage2.style.display = "none";
+    signupForm.style.display = "none";
+    dashboard.style.display = "block";
+}
